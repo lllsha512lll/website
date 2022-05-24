591567ce362800e9209e8c939069e53bf629f7ab
lllsha512lll <lllsha512lll@gmail.com>
2022-05-24 03:03:40 -0600

    Added Navigation bar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {useState} from 'react'
+import {ImHome} from 'react-icons/im'
+import {TiInfoLargeOutline} from 'react-icons/ti'
+import './nav.css'
+
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    const [activeNav,setActiveNav] = useState('#')
+    return (
+      <nav>
+        <a href="#"
+           onClick={() => setActiveNav('#')}
+           className={activeNav === '#'?'active':''}>
+            <ImHome/>
+           </a>
+
+       <a href="#about" 
+         onClick={() => setActiveNav('#about')} 
+         className={activeNav === '#about'?'active':''}>
+           <TiInfoLargeOutline/>
+         </a>
+      </nav>
+    );
 }
 
 export default Nav
